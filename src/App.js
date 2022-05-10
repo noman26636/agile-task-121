18353227d04f6f86a0bfb3f3624da4191673ab69
noman26636 <83615163+noman26636@users.noreply.github.com>
2022-05-10 21:42:05 +0500

    Added components to appjs and react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 function App() {
+
+
   return (
     <>
-    
+    <Routes>
+     <Route exact path="/" element={<Home/>}/>
+    </Routes>
     </>
   );
 }
