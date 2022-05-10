d7d01e8a3590dc5512b02cea5df24d3a93f5710b
noman26636 <83615163+noman26636@users.noreply.github.com>
2022-05-10 21:17:49 +0500

    Added buttons, captions and input

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,6 +13,44 @@ function Post() {
         <p className="flex-1 font-bold">noman2</p>
         <DotsHorizontalIcon className="h-5" />
     </div>
+        {/*Image*/}
+
+        <img src="" className="object-cover w-full" alt=""/>
+
+{/*Buttons*/}
+
+      <div className='flex justify-between px-4 pt-4'>
+      <div className="flex space-x-4 ">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn"/>
+          <PaperAirplaneIcon className="btn rotate-45" /> 
+      </div>
+      <BookmarkIcon className="btn"/>
+      </div>
+
+  {/*Captions*/}
+
+   <p className="p-5 truncate">
+        <span className="font-bold mr-1">noman2</span>
+        hello
+   </p>
+
+
+{/*Input Box*/}
+
+
+  <form className="flex items-center p-4">
+  <EmojiHappyIcon className="h-7 mr-5" />
+  <input type="text"
+  placeholder="Add a comment..." 
+  className="border-none flex-1 focus:ring-0 outline-none mr-4" 
+  />
+  <button 
+  type="submit" 
+  className="font-semibold text-blue-400">Post</button>
+  </form>
+
+
 </div>
   )};
     
