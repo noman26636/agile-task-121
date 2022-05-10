7962f95bf4447d30ae7c7edf09f25b833458bdd3
noman26636 <83615163+noman26636@users.noreply.github.com>
2022-05-10 21:15:54 +0500

    Created header for post

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+import { BookmarkIcon, ChatIcon,DotsHorizontalIcon,
+    EmojiHappyIcon,HeartIcon,PaperAirplaneIcon} from '@heroicons/react/outline'
+import { useEffect } from 'react';
+
+function Post() {
+     
+  return (
+    <div className="bg-white my-8 border rounded-sm">
+    {/*Header*/}
+    
+        <div className="flex items-center p-5">
+        <img className="rounded-full h-12 w-12 object-contain border p-1 mr-3" src="" alt="" />
+        <p className="flex-1 font-bold">noman2</p>
+        <DotsHorizontalIcon className="h-5" />
+    </div>
+</div>
+  )};
+    
+export default Post
